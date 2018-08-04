a9032189412dcca5149aaefb6c55efef4a710095
Daniel Murillo <danielmurillo@Daniels-MacBook-Pro.local>
2018-08-03 22:32:42 -0700

    added pages and Set up Nav we can start linking pages

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
+  // Gets all horses
   getHorses: function() {
     return axios.get("/api/horses");
   },
-  // Gets the book with the given id
+  // Gets the horse with the given id
   getHorse: function(id) {
     return axios.get("/api/horses/" + id);
   },
-  // Deletes the book with the given id
+  // Deletes the horse with the given id
   deleteHorse: function(id) {
     return axios.delete("/api/horses/" + id);
   },
-  // Saves a book to the database
+  // Saves a horse to the database
   saveHorse: function(horseData) {
     return axios.post("/api/horses", horseData);
   }
