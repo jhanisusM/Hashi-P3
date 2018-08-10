4e4b00dba6ecaa37a48fa1f357112e6af2946d13
Jaylee Maruk <jayleetcu@gmail.com>
2018-08-10 16:09:59 -0700

    search

--- client/src/Pages/SearchHorses/SearchHorses.js
+++ client/src/Pages/SearchHorses/SearchHorses.js
@@ -1,74 +1,83 @@
-// import React, { Component } from "react";
-// import API from "../../utils/API";
-// import { Link } from "react-router-dom";
-// import { Col, Row, Container } from "../../components/Grid";
-// import Jumbotron from "../../components/Jumbotron";
-// import { List, ListItem } from "../../components/List";
-// import { Input, TextArea, FormBtn } from "../../components/Form";
-// import DeleteBtn from "../../components/DeleteBtn";
-// import { throws } from "assert";
-
-
-// class SearchHorses extends Component {
-
-//     state = {
-//         horses: [],
-//         foundHorse: [],
-//         name: "",
-//         sire: "",
-//         mare: "",
-//         gender: "",
-//         age: "",
-//     };
-
-//     componentDidMount() {
-//         this.loadHorses();
-//     }
-
-//     loadHorses = () => {
-//         API.getHorses()
-//             .then(res =>
-//                 this.setState({ horses: res.data, foundHorse: [], name: "", sire: "", mare: "", gender: "", age: "" })
-//             )
-//             .catch(err => console.log(err));
-//     };
-
-//     searchResults = () => {
-//         const search = this.state.horses.filter(horse => (horse.age >= 5));
-//         return (
-//             <List>
-//                 {weanlings.map(horse => (
-//                     <ListItem key={horse._id}>
-//                         <Link to={"/horses/" + horse._id}>
-//                             <strong>
-//                                 {horse.name} by {horse.sire}
-//                             </strong>
-//                         </Link>
-//                         <DeleteBtn onClick={() => this.deleteHorse(horse._id)} />
-//                     </ListItem>
-//                 ))}
-//             </List>
-
-//         )
-//     };
+import React, { Component } from "react";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components/Grid";
+import Jumbotron from "../../components/Jumbotron";
+import { List, ListItem } from "../../components/List";
+import { Input, TextArea, FormBtn } from "../../components/Form";
+import DeleteBtn from "../../components/DeleteBtn";
+import { throws } from "assert";
+import "./SearchHorses.css";
 
 
 
- 
-//         render() {
-//             return (
-//               <Form>
-//                 <FormGroup>
-//                   <Label for="exampleEmail">Text</Label>
-//                   <Input type="text" name="email" id="exampleEmail" placeholder="with a placeholder" />
-//                 </FormGroup>
-//                 <Button>Submit</Button>
-//               </Form>
-//             );
-//           }
-        
+class SearchHorses extends Component {
+
+    state = {
+        horses: [],
+        foundHorse: [],
+        name: "",
+        sire: "",
+        mare: "",
+        gender: "",
+        age: "",
+    };
+
+    componentDidMount() {
+        this.loadHorses();
+    }
+
+    loadHorses = () => {
+        API.getHorses()
+            .then(res =>
+                {console.log(res)
+                this.setState({ horses: res.data, foundHorse: [], name: "", sire: "", mare: "", gender: "", age: "" })
+                })
+            .catch(err => console.log(err));
+    };
+
+    searchResults = () => {
+        const search = this.state.horses.filter(horse => (horse.age >= 0));
+        return (
+            <List>
+                {search.map(horse => (
+                    <ListItem key={horse._id}>
+                        <Link to={"/horses/" + horse._id}>
+                            <strong>
+                                {horse.name} by {horse.sire}
+                            </strong>
+                        </Link>
+                        <DeleteBtn onClick={() => this.deleteHorse(horse._id)} />
+                    </ListItem>
+                ))}
+            </List>
+
+        )
+    };
 
 
-// };
 
-// export default SearchHorses;
+
+    render() {
+        return (
+            <div id="searchformcontainder">
+                <form id="searchform">
+
+                    <label htmlFor="search">Search</label>
+                    <input type="text" name="email" id="search" placeholder="Horse Name" />
+
+                    <button>Submit</button>
+                </form>
+                <div>
+                    {this.searchResults()}
+                </div>
+
+            </div>
+        );
+    }
+
+
+
+};
+
+export default SearchHorses;
--- controllers/horsesControllers.js
+++ controllers/horsesControllers.js
@@ -4,7 +4,7 @@ const db = require("../model");
 module.exports = {
   findAll: function(req, res) {
     db.Horse
-      .find(req.query)
+      .find()
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
