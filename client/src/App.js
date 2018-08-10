c539603e88a11f735963c5650105b7196db081f6
Adam <ajgus15@gmail.com>
2018-08-09 19:20:06 -0700

    adam stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,27 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Horses from "./Pages/Horses";
-import Home from "../src/Pages/Home"
-import SearchHorses from "./Pages/SearchHorses";
-import AddHorse from "./Pages/AddHorse";
-import AllHorses from "./Pages/AllHorses";
-
-import Navbar from "./components/Nav"
-import Weanlings from "./Pages/Weanlings";
-import Yearlings from "./Pages/Yearlings";
+import Yearlings from './Pages/Yearlings'
+import Navbar from './components/Nav/Nav';
 
 
 const App = () => (
   <Router>
     <div>
       <Navbar />
-      <Switch>
-        {/*/<Horses /> */}
-        <Route  path="/" component={Home} />
-         
+
+        {/* <Route  path="/" component={Home} />
         <Route  path="/SearchHorses" component={SearchHorses} />
         <Route  path="/AddHorse" component={AddHorse} />
-      <Route  path="/AllHorses" component={AllHorses} /> 
-        {/* <Route path="/Weanlings" component={Weanlings} /> */}
-        {/* <Route  path="/Yearlings" component={Yearlings} />
+        <Route  path="/AllHorses" component={AllHorses} />
+        <Route  path="/Weanlings" component={Weanlings} />
         <Route  path="/About" component={About} /> */}
-      </Switch>
 
-    </div>
+        <Route  path="/Yearlings" component={Yearlings} />
+        <Route path='/AddHorse' component={Horses} />
+    </div>     
   </Router>
 );
 
 
-export default App;
-
-// This code is what we're going to use when we have the pages and components made later on
-// Not necessarily the components that will house what we want but thats there for now so we know what
-// is going where 
-
+export default App
